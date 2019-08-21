05e2224ee9625eff339bcdd0d9d9b888c6e60f24
rahuljain1310 <ee1170476@iitd.ac.in>
2019-08-21 19:40:44 +0530

    Change params

--- AxisDetection.py
+++ AxisDetection.py
@@ -3,14 +3,16 @@ import cv2
 import numpy as np
 
 ## Background Removal ##
-BgSubstract = cv2.createBackgroundSubtractorMOG2()
+BgSubstract = cv2.createBackgroundSubtractorMOG2(history=4000,varThreshold=32,detectShadows=False)
 # kernel = np.ones((5,5),np.uint8)
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 
 def BackgroundRemove (frame, noiseFilter = False):
+	# x = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel, iterations=2)
 	fgmask = BgSubstract.apply(frame)
 	if noiseFilter:
 		fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+	showFrame('BackGround Remove',fgmask)
 	return fgmask
 
 def showFrame (name,frame):
@@ -26,12 +28,13 @@ def getPoints(rho,theta):
 	y1 = int(y0 + 1000*a)
 	x2 = int(x0 + 1000*b)
 	y2 = int(y0 - 1000*a)
+	print ((x1,y1),(x2,y2))
 	return (x1,y1),(x2,y2)
-
 
 def AddHoughLines(iframe, frame):
 	edges = cv2.Canny(iframe,50,200,apertureSize = 3)
-	lines = cv2.HoughLines(edges,1,np.pi/180,200)
+	Transformed = cv2.morphologyEx(edges, cv2.MORPH_OPEN, kernel)
+	lines = cv2.HoughLines(Transformed,1,np.pi/180,200)
 	showFrame('Edges',edges)
 	try:
 		for line in lines:
@@ -46,10 +49,11 @@ if __name__ == "__main__":
 	vidObj = cv2.VideoCapture(VideoPath)
 	ret, frame = vidObj.read()
 	while(ret):
-		frame = cv2.resize(frame, (960, 540))
-		iframe = BackgroundRemove(frame,noiseFilter=True)
-		AddHoughLines(iframe,frame)
-		if showFrame('Video', frame):
+		fr = cv2.resize(frame, (960, 540))
+		blur = cv2.GaussianBlur(fr,(25,25),0)
+		iframe = BackgroundRemove(blur,noiseFilter=True)
+		AddHoughLines(iframe,fr)
+		if showFrame('Video', fr):
 			break
 		ret, frame = vidObj.read()
 
