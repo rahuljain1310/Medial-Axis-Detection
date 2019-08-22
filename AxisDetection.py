e28da25d7167456bc8066238cf176272de4e994a
rahuljain1310 <ee1170476@iitd.ac.in>
2019-08-22 22:42:07 +0530

    Improved

--- AxisDetection.py
+++ AxisDetection.py
@@ -2,18 +2,32 @@
 import cv2
 import numpy as np
 
-## Background Removal ##
-BgSubstract = cv2.createBackgroundSubtractorMOG2(history=4000,detectShadows=False)
-kernel = np.ones((5,5),np.uint8)
-kernelMorph = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+## Constant ##
+Cyan = (255,255,0)
 
-def BackgroundRemove (frame, noiseFilter = False, blur = False):
-	if blur:
-		frame = cv2.GaussianBlur(frame,(3,3),0)
+## Background Removal ##
+# BgSubstract = cv2.createBackgroundSubtractorMOG2(history=8000,detectShadows=False)
+BgSubstract = cv2.createBackgroundSubtractorKNN(history=12000,detectShadows=False)
+kernel = np.ones((3,3),np.uint8)
+kernelBig = np.ones((5,5),np.uint8)
+kernelMorph = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+kernelRect = np.array([
+	[0,0,1,0,0],
+	[0,0,1,0,0],
+	[0,1,1,1,0],
+	[0,0,1,0,0],
+	[0,0,1,0,0]
+],dtype=np.uint8)
+
+def BackgroundRemove (frame):
+	frame = cv2.GaussianBlur(frame,(3,3),0)
 	fgmask = BgSubstract.apply(frame)
-	if noiseFilter:
-		fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernelMorph)
-		# fgmask = cv2.erode(fgmask,kernel)
+	# fgmask = cv2.dilate(fgmask,kernel)
+	fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+	fgmask = cv2.erode(fgmask,kernel,iterations=2)
+	fgmask = cv2.dilate(fgmask,kernelRect, iterations =2)
+	fgmask = cv2.erode(fgmask,kernel,iterations=1)
+	# fgmask = cv2.morphologyEx(fgmask,cv2.MORPH_CLOSE ,kernelBig, iterations=2)
 	showFrame('BackGround Remove',fgmask)
 	return fgmask
 
@@ -30,20 +44,24 @@ def getPoints(rho,theta):
 	y1 = int(y0 + 1000*a)
 	x2 = int(x0 + 1000*b)
 	y2 = int(y0 - 1000*a)
-	# print ((x1,y1),(x2,y2))
 	return (x1,y1),(x2,y2)
 
 def AddHoughLines(iframe, frame):
-	edges = cv2.Canny(iframe,100,200,apertureSize = 3)
-	# edges = cv2.Sobel(iframe,ddepth=-1,dx=1,dy=1)
-	lines = cv2.HoughLines(edges,2,np.pi/180,200)
+	edges = cv2.Canny(iframe,50,100,apertureSize = 3)
+	# edges = cv2.Sobel(iframe,ddepth=-5,dx=1,dy=1)
+	lines = cv2.HoughLines(edges,1,np.pi/180,100)
 	showFrame('Edges',edges)
 	try:
+		# z = [sum(y) / len(y) for y in zip(*lines)]
+		# print(lines)
+		# print(z)
+		# p1,p2 = getPoints(z[0],z[1])
+		# cv2.line(frame,p1,p2,Cyan,2)
 		for line in lines:
 			print(line)
 			for rho,theta in line:
 				p1,p2 = getPoints(rho,theta)
-				cv2.line(frame,p1,p2,(255,255,0),2)
+				cv2.line(frame,p1,p2,(0,255,255),2)
 	except:
 		pass
 	
@@ -52,14 +70,14 @@ if __name__ == "__main__":
 	vidObj = cv2.VideoCapture(VideoPath)
 	ret, frame = vidObj.read()
 	while(ret):
+		# fr = frame
 		fr = cv2.resize(frame, (960, 540))
 		gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
-		blank_image = np.zeros(shape=fr.shape, dtype=np.uint8)
-		iframe = BackgroundRemove(gray, noiseFilter=True, blur=False)
-		AddHoughLines(iframe,blank_image)
+		iframe = BackgroundRemove(gray)
+		AddHoughLines(iframe,fr)
 		# axisImage = cv2.bitwise_and(fr,blank_image,mask=iframe)
 		# axisFrame = cv2.add(axisImage,)
-		cv2.imshow('Video',cv2.add(fr,blank_image))
+		cv2.imshow('Video',fr)
 		if 27 == (cv2.waitKey(20) & 0xff):
 			break
 		ret, frame = vidObj.read()
