02e99b988d4c1b4292bc1226de4833db673a88ff
rahuljain1310 <ee1170476@iitd.ac.in>
2019-08-22 19:46:50 +0530

    Change

--- AxisDetection.py
+++ AxisDetection.py
@@ -3,15 +3,17 @@ import cv2
 import numpy as np
 
 ## Background Removal ##
-BgSubstract = cv2.createBackgroundSubtractorMOG2(history=4000,varThreshold=32,detectShadows=False)
-# kernel = np.ones((5,5),np.uint8)
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+BgSubstract = cv2.createBackgroundSubtractorMOG2(history=4000,detectShadows=False)
+kernel = np.ones((5,5),np.uint8)
+kernelMorph = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 
-def BackgroundRemove (frame, noiseFilter = False):
-	# x = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel, iterations=2)
+def BackgroundRemove (frame, noiseFilter = False, blur = False):
+	if blur:
+		frame = cv2.GaussianBlur(frame,(3,3),0)
 	fgmask = BgSubstract.apply(frame)
 	if noiseFilter:
-		fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+		fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernelMorph)
+		# fgmask = cv2.erode(fgmask,kernel)
 	showFrame('BackGround Remove',fgmask)
 	return fgmask
 
@@ -28,16 +30,17 @@ def getPoints(rho,theta):
 	y1 = int(y0 + 1000*a)
 	x2 = int(x0 + 1000*b)
 	y2 = int(y0 - 1000*a)
-	print ((x1,y1),(x2,y2))
+	# print ((x1,y1),(x2,y2))
 	return (x1,y1),(x2,y2)
 
 def AddHoughLines(iframe, frame):
-	edges = cv2.Canny(iframe,50,200,apertureSize = 3)
-	Transformed = cv2.morphologyEx(edges, cv2.MORPH_OPEN, kernel)
-	lines = cv2.HoughLines(Transformed,1,np.pi/180,200)
+	edges = cv2.Canny(iframe,100,200,apertureSize = 3)
+	# edges = cv2.Sobel(iframe,ddepth=-1,dx=1,dy=1)
+	lines = cv2.HoughLines(edges,2,np.pi/180,200)
 	showFrame('Edges',edges)
 	try:
 		for line in lines:
+			print(line)
 			for rho,theta in line:
 				p1,p2 = getPoints(rho,theta)
 				cv2.line(frame,p1,p2,(255,255,0),2)
@@ -50,10 +53,14 @@ if __name__ == "__main__":
 	ret, frame = vidObj.read()
 	while(ret):
 		fr = cv2.resize(frame, (960, 540))
-		blur = cv2.GaussianBlur(fr,(25,25),0)
-		iframe = BackgroundRemove(blur,noiseFilter=True)
-		AddHoughLines(iframe,fr)
-		if showFrame('Video', fr):
+		gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+		blank_image = np.zeros(shape=fr.shape, dtype=np.uint8)
+		iframe = BackgroundRemove(gray, noiseFilter=True, blur=False)
+		AddHoughLines(iframe,blank_image)
+		# axisImage = cv2.bitwise_and(fr,blank_image,mask=iframe)
+		# axisFrame = cv2.add(axisImage,)
+		cv2.imshow('Video',cv2.add(fr,blank_image))
+		if 27 == (cv2.waitKey(20) & 0xff):
 			break
 		ret, frame = vidObj.read()
 
