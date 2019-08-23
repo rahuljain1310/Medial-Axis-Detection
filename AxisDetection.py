2f228d432ba4afffb2169c1f42f810432a5fb78e
Vishwajeet Agrawal <vishwa.grawal@gmail.com>
2019-08-23 22:56:35 +0530

    axis d

--- AxisDetection.py
+++ AxisDetection.py
@@ -26,7 +26,7 @@ kernelRect = np.array([
 ],dtype=np.uint8)
 
 def BackgroundRemove (frame):
-	# frame = cv2.GaussianBlur(frame,(3,3),0)
+	frame = cv2.GaussianBlur(frame,(7,7),0)
 	fgmask = BgSubstract.apply(frame)
 	fgmask = cv2.dilate(fgmask,kernel2)
 	fgmask = cv2.dilate(fgmask,kernel1,iterations = 2)
@@ -162,36 +162,38 @@ def getMedialLine(lines):
 	return rho,theta
 
 def getHoughLines(iframe):
-	edges = cv2.Canny(iframe,50,100,apertureSize = 7)
+	edges = cv2.Canny(iframe,30,70)
 	# edges = iframe
 	# edges = cv2.Sobel(iframe,ddepth=-5,dx=1,dy=1)
 	showFrame('Edges',edges)
-	lines = cv2.HoughLines(edges,1,np.pi/180,90)
+	lines = cv2.HoughLines(edges,1,np.pi/180,60)
 	linesf1,theta = theta_filter(lines)
 	linesf2,rho,linesf3,rho2 = rho_filter(linesf1) 
-
-	Segments = cv2.HoughLinesP(edges,rho = 1,theta = np.pi/180,threshold = 75,minLineLength=20,maxLineGap=10)
+	#print(linesf2,linesf3)
+	Segments = cv2.HoughLinesP(edges,rho = 1,theta = np.pi/180,threshold = 20,minLineLength=5,maxLineGap=15)
 	def segf(segment):
 		x1,y1,x2,y2 = segment[0][0],segment[0][1],segment[0][2],segment[0][3]
 		return segment_filter(x1,y1,x2,y2,rho,theta) or segment_filter(x1,y1,x2,y2,-rho,theta)
 	def segf2(segment):
 		x1,y1,x2,y2 = segment[0][0],segment[0][1],segment[0][2],segment[0][3]
 		return segment_filter(x1,y1,x2,y2,rho2,theta) or segment_filter(x1,y1,x2,y2,-rho2,theta)
-	linesf = []
-	segmentsf4 = []
+	
+	segmentsf4 = None
 	if (Segments.__class__==np.ndarray and linesf2.__class__==list and linesf3.__class__==list):
 		#print(rho,theta)
 		#print(len(Segments))
 		segmentsf1 = list(filter(segf,Segments))
 		segmentsf2 = list(filter(segf2,Segments))
 		#print(len(segmentsf1))
-		linesf = linesf2+linesf3
+		
 		segmentsf4 = segmentsf1 + segmentsf2
 
 	else:
+		
 		segmentsf1 = None
-
-	return (linesf,segmentsf4)
+	if (segmentsf4 == []):
+		segmentsf4 =None
+	return (linesf2,linesf3,segmentsf4)
 
 def getPoints(rho,theta):
 	a = np.cos(theta)
@@ -217,6 +219,7 @@ def AddHoughSegments(Segments,frame):
 	try:
 		for c in Segments:
 			for x1,y1,x2,y2 in c:
+				# print(x1,y1,x2,y2)
 				cv2.line(frame,(x1,y1),(x2,y2),(255,165,0),2)
 	except:
 		pass
@@ -234,53 +237,45 @@ def getYBoundary(Segments):
 	#print(picks)
 	ymin = st.mean(ys[:picks])
 	ymax = st.mean(ys[ll-1-picks:]) 
-	#print(ymin,ymax,10000)
-	# yhists = np.histogram(ys,10)
-	# ymin = st.median(yhists[0])
-	# ymax = st.median(yhists[9])
+	
 	return ymin,ymax
-	# ymin = math.inf
-	# ymax = 0
-	# for s in Segments:
-	# 	for _,y1,_,y2 in s:
-	# 		if y2<y1:
-	# 			t = y1
-	# 			y1 = y2
-	# 			y2 = t
-	# 		if y1<ymin:
-	# 			ymin = y1
-	# 		if y2>ymax:
-	# 			ymax = y2
-	# return ymin,ymax
+	
 
 def getMedianLineSegment(rho,theta,Ymin,Ymax):
 	if theta == 0:
-		return (rho,int(Ymin)),(rho,int(Ymax))
+		return (int(rho),int(Ymin)),(int(rho),int(Ymax))
 	Xmin = int(rho/np.cos(theta) - Ymin*np.tan(theta))
 	Xmax = int(rho/np.cos(theta) - Ymax*np.tan(theta))
 	return (Xmin,int(Ymin)),(Xmax,int(Ymax))	
 
 if __name__ == "__main__":
-	VideoPath = "3.mp4"
+	VideoPath = input()
 	vidObj = cv2.VideoCapture(VideoPath)
 	ret, frame = vidObj.read()
 	while(ret):
 		fr = cv2.resize(frame, (960, 540))
-		gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
-		iframe = BackgroundRemove(gray)
-		lines,Segments = getHoughLines(iframe)
-		try:
+		# gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+		iframe = BackgroundRemove(fr)
+		lines1,lines2,Segments = getHoughLines(iframe)
+		#print(lines1.__class__,lines2.__class__)
+		if (lines1.__class__==list and lines2.__class__==list and Segments.__class__==list):
+			# print(Segments)
 			# AddHoughLines(lines,fr)
 			
 			# AddHoughSegments(Segments,fr)
-			rho,theta = getAverageLine(lines)
+			# rho,theta = getAverageLine(lines)
+			rho1,theta1 = getAverageLine(lines1)
+			rho2,theta2 = getAverageLine(lines2)
+			rho = (rho1+rho2)//2
+			theta = (theta1+theta2)/2
 			Ymin,Ymax = getYBoundary(Segments)
 			p1,p2 = getMedianLineSegment(rho,theta,Ymin,Ymax)
-			print(p1,p2)
-			print(lines[0],'3')
+			# print(p1,p2)
+			# print(lines[0],'3')
 			cv2.line(fr,(p1),(p2),(0,0,255),1)
-			print("Success")
-		except:
+			# print("Success")
+		else:
+			# print('error')
 			pass
 		cv2.imshow('Video',fr)
 		if 27 == (cv2.waitKey(20) & 0xff):
@@ -292,20 +287,3 @@ vidObj.release()
 cv2.destroyAllWindows()
 
 
-def length(v):
-    return sqrt(v[0]**2+v[1]**2)
-def dot_product(v,w):
-   return v[0]*w[0]+v[1]*w[1]
-def determinant(v,w):
-   return v[0]*w[1]-v[1]*w[0]
-def inner_angle(v,w):
-   cosx=dot_product(v,w)/(length(v)*length(w))
-   rad=np.arccos(cosx) # in radians
-   return rad*180/np.pi # returns degrees
-def angle_clockwise(A, B):
-    inner=inner_angle(A,B)
-    det = determinant(A,B)
-    if det<0: #this is a property of the det. If the det < 0 then B is clockwise of A
-        return inner
-    else: # if the det > 0 then A is immediately clockwise of B
-        return 360-inner
